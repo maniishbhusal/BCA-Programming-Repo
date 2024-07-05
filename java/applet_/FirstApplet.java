418431e5f9abb48c5a9a98d1952f3b03794e2512
Manish Bhusal <manishbhusal6@gmail.com>
2024-07-05 10:11:26 +0545

    learn about applet

---
+++ java/applet_/FirstApplet.java
@@ -0,0 +1,9 @@
+import java.applet.Applet;
+import java.awt.Graphics;
+
+public class FirstApplet extends Applet {
+
+    public void paint(Graphics g) {
+        g.drawString("Hello World", 50, 50);
+    }
+}
