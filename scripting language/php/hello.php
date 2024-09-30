8a941230e72ad95b34b3e47668e14910f19311bc
Manish Bhusal <84217955+maniishbhusal@users.noreply.github.com>
2024-09-30 08:42:36 +0545

    Create hello.php

---
+++ scripting language/php/hello.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>first hello world program</h1>
+    <?php
+    echo "hello world"."<br>";
+
+    $color = 'red';
+    echo "my car is $color" ."<br>";
+    echo "this is color " .$color ."<br>";
+
+    $text="Nepal";
+    echo "I love Anu $text";
+
+    $x= 5;
+    $y= "John";
+    echo $x;
+    echo $y;
+
+    var_dump(5);
+    var_dump("John");
+    var_dump(3.14);
+    var_dump(true);
+    var_dump([2,3,56]);
+    var_dump(NULL);
+    ?>
+</body>
+</html>
